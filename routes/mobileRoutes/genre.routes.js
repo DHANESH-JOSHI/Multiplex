55f844090421c5c9c6040e897dd401caba2cb2ec
ruby <rubypilkhwal04@gmail.com>
2025-04-04 17:29:35 +0530

    "UPDATE: Route_gener"

--- routes/mobileRoutes/genre.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getAllGenres } = require("../../controllers/mobileControllers/allgenre.controller");
-
-router.get("/", getAllGenres);
-
-module.exports = router;
