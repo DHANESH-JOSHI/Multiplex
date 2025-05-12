afb9520ff7303fbc4bafded5dbd50d3526a63a44
DHANESHJOSHI <hydradhanesh@gmail.com>
2025-05-12 21:30:01 +0530

    updated status Controller

--- routes/adminRoutes/status.routes.js
+++ routes/adminRoutes/status.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { getStatus, updatestatus } = require("../../controllers/adminController/adminAuth.controller");
+const { getStatus, updatestatus } = require("../../controllers/adminController/status.controller");
 
 router.get("/", getStatus);
 router.post("/:id", updatestatus);
