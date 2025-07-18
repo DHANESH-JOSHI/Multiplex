eb905a3738a70a4411232c7c49cafc79d06ea14c
DHANESHJOSHI <hydradhanesh@gmail.com>
2025-07-18 15:52:40 +0530

    ===> updated

--- routes/mobileRoutes/movies.routes.js
+++ routes/mobileRoutes/movies.routes.js
@@ -6,8 +6,8 @@ const  {upload, deleteFileAfterResponse} = require("../../middleware/multer");
 
 
 // Routes
-router.get("/", cacheMiddleware(0),deleteFileAfterResponse, MovieController.getAllMovies);   
-router.get("/single-movie", cacheMiddleware(0), MovieController.getMovieById);
+router.get("/",deleteFileAfterResponse, MovieController.getAllMovies);    // cacheMiddleware(0),
+router.get("/single-movie",  MovieController.getMovieById); //cacheMiddleware(0),
 router.post("/", upload.single('file'),deleteFileAfterResponse, MovieController.addMovie);
 router.post("/upload", upload.single('file'),deleteFileAfterResponse, MovieController.uploadOnly);
 router.put("/:id", MovieController.updateMovie);
