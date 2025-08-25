7aac374de5cf475d47b3e0e9576aa541bb61d086
DHANESHJOSHI <hydradhanesh@gmail.com>
2025-08-25 12:35:24 +0530

    ===> update

--- routes/mobileRoutes/homeContent.routes.js
+++ routes/mobileRoutes/homeContent.routes.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { getHomeContentForAndroid } = require("../../controllers/mobileControllers/homeContent.controller");
+const {
+  getHomeContentForAndroid,
+} = require("../../controllers/mobileControllers/homeContent.controller");
+const { cacheMiddleware } = require("../../middleware/nodeCache");
 
-router.get("/", getHomeContentForAndroid);
+router.get("/", cacheMiddleware(60), getHomeContentForAndroid);
 
 module.exports = router;
--- routes/mobileRoutes/movies.routes.js
+++ routes/mobileRoutes/movies.routes.js
@@ -2,14 +2,28 @@ const express = require("express");
 const router = express.Router();
 const MovieController = require("../../controllers/adminController/movie.controller");
 const { cacheMiddleware } = require("../../middleware/nodeCache");
-const  {upload, deleteFileAfterResponse} = require("../../middleware/multer");
-
+const { upload, deleteFileAfterResponse } = require("../../middleware/multer");
 
 // Routes
-router.get("/",deleteFileAfterResponse, MovieController.getAllMovies);    // cacheMiddleware(0),
-router.get("/single-movie",  MovieController.getMovieById); //cacheMiddleware(0),
-router.post("/", upload.single('file'),deleteFileAfterResponse, MovieController.addMovie);
-router.post("/upload", upload.single('file'),deleteFileAfterResponse, MovieController.uploadOnly);
+router.get(
+  "/",
+  cacheMiddleware(60),
+  deleteFileAfterResponse,
+  MovieController.getAllMovies
+); // ,
+router.get("/single-movie", MovieController.getMovieById); //cacheMiddleware(0),
+router.post(
+  "/",
+  upload.single("file"),
+  deleteFileAfterResponse,
+  MovieController.addMovie
+);
+router.post(
+  "/upload",
+  upload.single("file"),
+  deleteFileAfterResponse,
+  MovieController.uploadOnly
+);
 router.put("/:id", MovieController.updateMovie);
 router.delete("/:id", MovieController.deleteMovie);
 
